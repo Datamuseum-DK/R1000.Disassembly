fd7c181f4865e3f6b968eaf9de3c7d6d0640628a
Poul-Henning Kamp <phk@FreeBSD.org>
2021-03-02 17:33:59 +0000

    More infrastructure

---
+++ DFS/all.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2012-2021 Poul-Henning Kamp <phk@phk.freebsd.dk>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+
+'''
+   All objects
+   -----------
+'''
+
+from pyreveng import data
+
+def round_0(cx):
+    ''' Things to do before the disassembler is let loose '''
+
+    # Look for SCCS-identifiers at the beginning
+    for a, b in cx.m.gaps():
+        for i in range(a, a + 0x500):
+            if cx.m.bu32(i) == 0x40282329:
+                y = data.Txt(cx.m, i, term=(0x5c,), label=False)
+
+def round_1(cx):
+    ''' Let the disassembler loose '''
+
+def round_2(cx):
+    ''' Spelunking in what we alrady found '''
+
+def round_3(cx):
+    ''' Discovery, if no specific hints were encountered '''
