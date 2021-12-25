de81d0c8ff8c5d7d6c79fd4fac7ff9d813fcf61f
Poul-Henning Kamp <phk@FreeBSD.org>
2021-12-25 22:58:17 +0000

    Some work to understand EXPMON

---
+++ DFS/ident/5dfdd8182e2a5ffb.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2021 Poul-Henning Kamp <phk@phk.freebsd.dk>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+
+'''
+   EXPMON.M200
+   -----------
+'''
+
+from pyreveng import data
+
+def round_0(cx):
+    ''' Things to do before the disassembler is let loose '''
+    for a, b, c in (
+        (0x20418, 0x2047c, 4),
+        (0x2048c, 0x204f8, 12),
+        (0x204f8, 0x2055c, 10),
+        (0x21784, 0x21946, 10),
+        (0x21b8a, 0x21d48, 10),
+    ):
+        for d in range(a, b, c):
+            y = data.Txt(cx.m, d, d + c, label=False)
+
+def round_1(cx):
+    ''' Let the disassembler loose '''
+
+def round_2(cx):
+    ''' Spelunking in what we alrady found '''
+
+def round_3(cx):
+    ''' Discovery, if no specific hints were encountered '''
+
+
