aee16cfb9e599c3d4fb131fdb7602f4d8b33b5be
Poul-Henning Kamp <phk@FreeBSD.org>
2023-04-04 06:53:14 +0000

    Bit of work on FIXMODEM.M200

---
+++ DFS/ident/4b33e791b23b11a9.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2012-2021 Poul-Henning Kamp <phk@phk.freebsd.dk>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+
+'''
+   FIXMODEM.M200 4b33e791b23b11a9
+   ------------------------------
+'''
+
+from pyreveng import assy, data
+
+def round_0(cx):
+    ''' Things to do before the disassembler is let loose '''
+
+    for i, j in (
+        (0x20584, "Status2Text(a: Long) : String"),
+        (0x20aac, "ReadConfig(adr: Long) : Byte"),
+        (0x2117a, "ConnectingModem()"),
+    ):
+        cx.m.set_label(i, j)
+
+    cx.m.set_line_comment(0x20cde, "Read Config.Cluster_Number")
+
+def round_1(cx):
+    ''' Let the disassembler loose '''
+
+def round_2(cx):
+    ''' Spelunking in what we already found '''
+
+def round_3(cx):
+    ''' Discovery, if no specific hints were encountered '''
